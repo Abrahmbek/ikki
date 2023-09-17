2ac22602ef59d39ca55316d753e08af839fc7cc3
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-09-17 18:47:04 +0900

    fix: all

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function countLetter(harf, soz) {            //ikkita parametrli funksiya elon qildik
-    let sanash = 0;                            // sanash ozgaruchisi orqali qoshib boradi
-    
-    
-    for (let i = 0; i < soz.length; i++) {         //   for operatoi orqali pastta berilgan                                                                                           // argumentni uzunligi boyicha harfma harf                                                                                           //  tekshiradi agar topsa sanashga 1 ta qoshib qoyadi
-      if (soz[i] === harf) {
-        sanash++;
-      }
-    }
-    
-    return sanash;
-  }
-  
-  const natija = countLetter("a", "abrahm, anvar, akmal"); // bu yerda natija ozgaruchisiga argumentlarimizni beramiz 
-  console.log(natija);                                      /// va log qilib natijani olamiz
+// file modullar
+
+const calculate = require("./hisob.js");
+
+const natija = calculate.kopaytirish(20,15);
+console.log("natija:", natija);
+
+console.log("========================");
+
+const natija2 = calculate.ayrish(20,15);
+console.log("natija:", natija2);
+
+console.log("========================");
+
+const natija3 = calculate.ayrish(20,15);
+console.log("natija:", natija3);
